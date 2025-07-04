7eaea0cb5925b277a2348c6f8e7c2bdc947829ee
farho <farhodovjasur99@gmail.com>
2025-07-05 01:43:31 +0500

    # 'django.middleware.csrf.CsrfViewMiddleware',

--- core/settings.py
+++ core/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -73,7 +73,8 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
-    'http://165.232.93.246:8000'
+    'http://165.232.93.246:8000',
+    "https://jjks.site",
 ]
 
 ROOT_URLCONF = 'core.urls'
