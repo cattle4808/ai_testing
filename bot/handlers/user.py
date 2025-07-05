8d4d0c3be53a3bbcf2351d8bdf3a7245d2d5343e
farho <farhodovjasur99@gmail.com>
2025-07-05 11:00:11 +0500

    add test responses2

--- bot/handlers/user.py
+++ bot/handlers/user.py
@@ -9,22 +9,27 @@ user = Router()
 
 @user.message(F.text == CommandMap.User.BUY_SCRIPT)
 async def buy_script(message: types.Message):
+    await message.delete()
     await message.answer("Выберите скрипт")
 
 @user.message(F.text == CommandMap.User.MY_SCRIPTS)
 async def my_scripts(message: types.Message):
+    await message.delete()
     await message.answer("Мои скрипты")
 
 @user.message(F.text == CommandMap.User.INSTRUCTION)
 async def instruction(message: types.Message):
+    await message.delete()
     await message.answer("Инструкция")
 
 @user.message(F.text == CommandMap.User.SUPPORT)
 async def support(message: types.Message):
+    await message.delete()
     await message.answer("Поддержка")
 
 @user.message(F.text == CommandMap.User.MY_DATA)
 async def my_data(message: types.Message):
+    await message.delete()
     await message.answer("Мои данные")
 
 
