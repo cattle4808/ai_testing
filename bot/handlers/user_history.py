e23ff209f919c997ccef6e5b4aa9a49650845aec
farho <farhodovjasur99@gmail.com>
2025-07-11 03:28:05 +0500

    user_history.py

--- bot/handlers/user_history.py
+++ bot/handlers/user_history.py
@@ -53,7 +53,7 @@ async def render_sessions_page(message: types.Message, scripts: list, page: int)
             f"🆔 <code>{s['key']}</code>\n"
             f"📜 {s['script']}\n"
             f"⏱ {s['start_at']} - {s['stop_at']}\n"
-            f"{'💰 Оплачено' if s['is_paid'] else '🚫 Не оплачено'}\n"
+            f"{'💰 Оплачено' if s['is_active'] else '🚫 Не оплачено'}\n"
             f"_____________\n\n"
         )
 
