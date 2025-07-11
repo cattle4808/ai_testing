97854f3734f4fe149bed8a7ed73cb7af9266aeb3
farho <farhodovjasur99@gmail.com>
2025-07-11 08:03:06 +0500

    user_history.py

--- api/bot/urls.py
+++ api/bot/urls.py
@@ -1,14 +1,19 @@
 from rest_framework.urls import path
-
 from api.bot import views
-
 
 urlpatterns = [
     path('webhook/', views.webhook),
+    path('webhook', views.webhook),
 
     path('create_script_select_time/', views.create_script_view),
+    path('create_script_select_time', views.create_script_view),
+
     path('select_time/', views.select_time),
+    path('select_time', views.select_time),
+
     path('redact_time/', views.redact_time),
+    path('redact_time', views.redact_time),
 
     path('change_time_view/', views.change_time_view),
+    path('change_time_view', views.change_time_view),
 ]
--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,14 +1,25 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('create_script/', views.CreateIdScriptApiView.as_view()),
-    path('get_or_create_tg_user/', views.GetOrCreateTgUserView.as_view()),
-    path('get_my_scripts/', views.GetMyScriptsView.as_view()),
-    path('change_script_isactive/<str:key>', views.ChangeIsActivateView.as_view()),
-    path('change_script_time/', views.ChangeScriptTimeView.as_view()),
-    path('get_script/<str:script>/', views.GetScript.as_view()),
-    path('check/', views.AiAnswerCheckView.as_view())
-]
+    path('create_script', views.CreateIdScriptApiView.as_view()),
 
+    path('get_or_create_tg_user/', views.GetOrCreateTgUserView.as_view()),
+    path('get_or_create_tg_user', views.GetOrCreateTgUserView.as_view()),
+
+    path('get_my_scripts/', views.GetMyScriptsView.as_view()),
+    path('get_my_scripts', views.GetMyScriptsView.as_view()),
+
+    path('change_script_isactive/<str:key>/', views.ChangeIsActivateView.as_view()),
+    path('change_script_isactive/<str:key>', views.ChangeIsActivateView.as_view()),
+
+    path('change_script_time/', views.ChangeScriptTimeView.as_view()),
+    path('change_script_time', views.ChangeScriptTimeView.as_view()),
+
+    path('get_script/<str:script>/', views.GetScript.as_view()),
+    path('get_script/<str:script>', views.GetScript.as_view()),
+
+    path('check/', views.AiAnswerCheckView.as_view()),
+    path('check', views.AiAnswerCheckView.as_view()),
+]
